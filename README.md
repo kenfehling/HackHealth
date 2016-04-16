427b230ae65464182c32e640c5e257b799e11c49
Ken Fehling <ken@androidideas.org>
2016-04-16 13:35:01 -0400

    Got selecting and answer working

---
+++ README.md
@@ -0,0 +1,4 @@
+## Setup
+
+1. Run `bower install`
+2. Open `index.html` in a browser
--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
 $(function() {
-    $('.food').click(function() { alert('soi')});
+    var clickSelector = '.food img.image';
+    var $answer = $('#answer');
+    var $foods = $('.food');
+
+    fetch('data.json')
+    .then(function(response) {
+        return response.json()
+    }).then(function(data) {
+        startGame(data);
+    });
+
+    function startGame(data) {
+        var foods = _.sampleSize(data, 2);
+
+        _.each(foods, function(food, i) {
+            var $el = $($foods.get(i));
+            $el.find('img.image').attr('src', 'img/' + food.img);
+            $el.find('img.image').attr('alt', 'img/' + food.name);
+            $el.find('.name').text(food.name);
+        });
+
+        $(clickSelector).click(function(e) {
+            var lowest = _.min(_.map(foods, function(f) { return f.sugar; }));
+            var index = $(e.target).index(clickSelector);
+            if (foods[index].sugar === lowest) {
+                $answer.addClass('correct');
+                $answer.text('Correct!');
+            }
+            else {
+                $answer.addClass('wrong');
+                $answer.text('Wrong');
+            }
+            _.each(foods, function(food, i) {
+                $($foods.get(i)).find('.sugar').text(food.sugar);
+            });
+        });
+    }
 });
